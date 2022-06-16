51a68af6a50b57e27668e9ba1d3755199d38b309
elyonousimohamed <m.el.yonousi@ibhorizons.com>
2022-06-16 16:14:54 +0100

    more learn

---
+++ src/com/casetrue/java_buttons_52/Main.java
@@ -0,0 +1,9 @@
+package com.casetrue.java_buttons_52;
+
+
+public class Main {
+    public static void main(String[] args) {
+        // JButton = a button that performs an action when clicked on
+        new MyFrame();
+    }
+}
